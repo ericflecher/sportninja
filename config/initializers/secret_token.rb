059bfdccaedb48c362ea2d1b811d728c79dfce06
ericflecher <eric.flecher@gmail.com>
2011-08-07 17:49:21 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportninja::Application.config.secret_token = '15ca0032217e5f3e8277ec82b7fae5e6a39babd5a0b7078e2aa3bd085aed110b3d1a671dde0df4e650dbf7a3dba343336963db7ba8a058d8a6c983cdf27f9b1c'
